ac468e7c001fc43bc03759bfb6e5b6434a426ff9
sevasu2 <sevasunishimura@gmail.com>
2018-10-17 03:12:58 +0000

    cocoon導入、不要ファイル削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :name_kana, :address, presence: true
+  validates :postal_code, presence: true
+  validates :tel_number, presence: true
 end
