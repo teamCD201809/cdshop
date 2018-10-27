24090790fc639996fa51858f678c09616bfc6492
Hiroshige02 <oajeilwi00@gmail.com>
2018-10-27 04:03:13 +0000

    廣重10/27住所選択、購入履歴画面変更後

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -1,14 +1,17 @@
 class User::OrdersController < ApplicationController
   def create
-    @order = Order.new(order_params)
+    @order = Order.new
+    binding.pry
+    a = DeliveryAddress.find(params[:c][:d])
+     binding.pry
+    @order.sub_post_code = a.delivery_address_postal_code
+     binding.pry
+    @order.sub_address = a.delivery_address
+    @order.user_id = current_user.id
     @order.save
     binding.pry
     redirect_to user_cart_items_path
+    binding.pry
   end
-
- private
-    def order_params
-      params.require(:order).permit(:sub_post_code, :sub_address)
-    end
 
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,5 @@
 class CartItem < ApplicationRecord
+	default_scope -> {order(created_at: :desc)}
 	belongs_to :title
 	belongs_to :user
     has_one :orders_item
